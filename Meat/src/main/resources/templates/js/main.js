10c376a6e9a33053784949170cd4ebd1e7512c3c
imchanu96 <cksdn9979@gmail.com>
2024-11-07 15:50:39 +0900

    메인 페이지 css, js, vue.js 적용중

--- Meat/src/main/resources/templates/js/main.js
+++ Meat/src/main/resources/templates/js/main.js
@@ -1,10 +1,28 @@
 function showCategoryFnc(){
-    categoryDetailTag = document.getElementById("category_detail_section");
+    var categoryDetailTag = document.getElementById("category_detail_section");
     categoryDetailTag.style.transition = "all 0.5s";
-    categoryDetailTag.style.height = "300%";
     categoryDetailTag.style.zIndex = 10;
     categoryDetailTag.style.opacity = 100;
-    categoryDetailTag.style.position = "relative";
+    
+    var categoryDetailTagList = document.getElementById("category_detail").children;
+    var categoryDetailTagHeight = categoryDetailTagList[0].getBoundingClientRect().height;
+
+    for (let i = 1; i < categoryDetailTagList.length; i++) {
+        var elementHeight = categoryDetailTagList[i]. getBoundingClientRect().height; 
+        if (elementHeight >= categoryDetailTagHeight) {
+            categoryDetailTagHeight = elementHeight;
+        }
+        
+    }
+
+    categoryDetailTag.style.height = categoryDetailTagHeight + "px";
+
+    var subCategoryWidth = document.getElementsByClassName("category_list")[0].getBoundingClientRect().width;
+    var subCategoryList = categoryDetailTag.children[1].children; //서브 카테고리 리스트
+    for (let i = 0; i < subCategoryList.length; i++) {
+        subCategoryList[i].style.width = subCategoryWidth + "px";
+        
+    }
 
     categoryDetailTag.addEventListener("mouseleave", function(){
         categoryDetailTag.style.height = "0";
